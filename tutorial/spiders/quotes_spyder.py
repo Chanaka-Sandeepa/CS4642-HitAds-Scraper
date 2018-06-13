0a7262ee143e0243c7f8d110be61752b012b3812
Chanaka-Sandeepa <chanaka.14@cse.mrt.ac.lk>
2018-06-13 09:20:01 +0530

    hitads cars xtended to 1000

--- tutorial/spiders/quotes_spyder.py
+++ tutorial/spiders/quotes_spyder.py
@@ -6,6 +6,7 @@ class QuotesSpider(scrapy.Spider):
     name = "HitAds_Spyder"
 
     def start_requests(self):
+
         urls = [
             # 'http://www.trc.gov.lk/2014-05-13-13-23-17/events/755-15th-apt-telecommunication-ict-development-forum.html',
             # 'http://www.trc.gov.lk/world-telecommunication-information-society-day-2018.html',
@@ -28,14 +29,32 @@ class QuotesSpider(scrapy.Spider):
             # 'http://www.trc.gov.lk/2014-05-12-13-20-23/tariff-regulation.html',
             # 'http://www.trc.gov.lk/2014-05-12-13-20-23/telco-levies.html',
             'http://www.hitad.lk/EN/cars',
+            # 'http://www.hitad.lk/EN/cars?page=25',
+            # 'http://www.hitad.lk/EN/cars?page=50',
+            # 'http://www.hitad.lk/EN/cars?page=75',
+            # 'http://www.hitad.lk/EN/cars?page=100',
+            # 'http://www.hitad.lk/EN/cars?page=125',
+            # 'http://www.hitad.lk/EN/cars?page=150',
+            # 'http://www.hitad.lk/EN/cars?page=175',
+            # 'http://www.hitad.lk/EN/cars?page=200',
+            # 'http://www.hitad.lk/EN/cars?page=225',
+            # 'http://www.hitad.lk/EN/cars?page=250',
+            # 'http://www.hitad.lk/EN/cars?page=275',
+            # 'http://www.hitad.lk/EN/cars?page=300',
         ]
+        for i in range (25, 1000, 25):
+            domain_url = 'http://www.hitad.lk/EN/cars?page='
+
+            new_url = domain_url +str(i)
+            urls.append(new_url)
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        yield {
+        for ads in response.css('ul.cat-ads'):
+            yield {
 
-            'title': response.css('h4.fw_b::text').extract(),
-            'location': response.css('div.item-facets2 > font.hidden-xs::text').extract(),
-            'price': response.css('span.list-price-value::text').extract(),
-        }
+                'title': ads.css('h4.fw_b::text').extract_first(),
+                'location': ads.css('div.item-facets2 > font.hidden-xs::text').extract_first(),
+                'price': ads.css('span.list-price-value::text').extract_first(),
+            }
