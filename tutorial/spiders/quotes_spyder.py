8507adec6903043658443c724b68805a4e8c826b
Chanaka-Sandeepa <chanaka.14@cse.mrt.ac.lk>
2018-06-13 08:59:50 +0530

    hitads cars

--- tutorial/spiders/quotes_spyder.py
+++ tutorial/spiders/quotes_spyder.py
@@ -3,30 +3,31 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "TRC_Services"
+    name = "HitAds_Spyder"
 
     def start_requests(self):
         urls = [
-            'http://www.trc.gov.lk/2014-05-13-13-23-17/events/755-15th-apt-telecommunication-ict-development-forum.html',
-            'http://www.trc.gov.lk/world-telecommunication-information-society-day-2018.html',
-            'http://www.trc.gov.lk/notice-to-cable-tv-and-direct-to-home-satellite-tv-service-providers.html',
-            'http://www.trc.gov.lk/mr-p-r-s-p-jayathilake-is-the-new-director-general-of-trcsl.html',
-            'http://www.trc.gov.lk/2014-05-13-13-23-17/events/662-his-excellency-the-president-of-sri-lanka-graced-the-17th-apt-policy-and-regulatory-forum.html',
-            'http://www.trc.gov.lk/nisadiya.html',
-            'http://www.trc.gov.lk/2014-05-13-03-46-49/right-to-information.html',
-            'http://www.trc.gov.lk/spectrum-management/spectrum-management-in-sri-lanka.html',
-            'http://www.trc.gov.lk/2014-08-13-11-31-28/numbering.html',
-            'http://www.trc.gov.lk/2014-08-13-11-31-28/network-equipment/import-clearance.html',
-            'http://www.trc.gov.lk/2014-08-13-11-31-28/guidelines.html',
-            'http://www.trc.gov.lk/2014-05-12-12-36-13/system-licence/licensing-procedure-issuance-renewal.html',
-            'http://www.trc.gov.lk/2014-05-12-12-36-13/system-licence/licenced-operator-list.html',
-            'http://www.trc.gov.lk/2014-05-12-12-36-13/system-licence/public-notice-on-issuence-renewal-modification-of-licences.html',
-            'http://www.trc.gov.lk/2014-05-12-12-36-13/system-licence/public-consultation.html',
-            'http://www.trc.gov.lk/2014-05-12-12-36-13/frequency-licence.html',
-            'http://www.trc.gov.lk/2014-05-12-12-36-13/2014-05-12-13-18-07/list-of-licenced-vendors.html',
-            'http://www.trc.gov.lk/2014-05-12-12-36-13/2014-05-12-13-18-07/vendor-licence-procedure.html',
-            'http://www.trc.gov.lk/2014-05-12-13-20-23/tariff-regulation.html',
-            'http://www.trc.gov.lk/2014-05-12-13-20-23/telco-levies.html',
+            # 'http://www.trc.gov.lk/2014-05-13-13-23-17/events/755-15th-apt-telecommunication-ict-development-forum.html',
+            # 'http://www.trc.gov.lk/world-telecommunication-information-society-day-2018.html',
+            # 'http://www.trc.gov.lk/notice-to-cable-tv-and-direct-to-home-satellite-tv-service-providers.html',
+            # 'http://www.trc.gov.lk/mr-p-r-s-p-jayathilake-is-the-new-director-general-of-trcsl.html',
+            # 'http://www.trc.gov.lk/2014-05-13-13-23-17/events/662-his-excellency-the-president-of-sri-lanka-graced-the-17th-apt-policy-and-regulatory-forum.html',
+            # 'http://www.trc.gov.lk/nisadiya.html',
+            # 'http://www.trc.gov.lk/2014-05-13-03-46-49/right-to-information.html',
+            # 'http://www.trc.gov.lk/spectrum-management/spectrum-management-in-sri-lanka.html',
+            # 'http://www.trc.gov.lk/2014-08-13-11-31-28/numbering.html',
+            # 'http://www.trc.gov.lk/2014-08-13-11-31-28/network-equipment/import-clearance.html',
+            # 'http://www.trc.gov.lk/2014-08-13-11-31-28/guidelines.html',
+            # 'http://www.trc.gov.lk/2014-05-12-12-36-13/system-licence/licensing-procedure-issuance-renewal.html',
+            # 'http://www.trc.gov.lk/2014-05-12-12-36-13/system-licence/licenced-operator-list.html',
+            # 'http://www.trc.gov.lk/2014-05-12-12-36-13/system-licence/public-notice-on-issuence-renewal-modification-of-licences.html',
+            # 'http://www.trc.gov.lk/2014-05-12-12-36-13/system-licence/public-consultation.html',
+            # 'http://www.trc.gov.lk/2014-05-12-12-36-13/frequency-licence.html',
+            # 'http://www.trc.gov.lk/2014-05-12-12-36-13/2014-05-12-13-18-07/list-of-licenced-vendors.html',
+            # 'http://www.trc.gov.lk/2014-05-12-12-36-13/2014-05-12-13-18-07/vendor-licence-procedure.html',
+            # 'http://www.trc.gov.lk/2014-05-12-13-20-23/tariff-regulation.html',
+            # 'http://www.trc.gov.lk/2014-05-12-13-20-23/telco-levies.html',
+            'http://www.hitad.lk/EN/cars',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -34,7 +35,7 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         yield {
 
-            'title': response.css('title::text').extract_first(),
-            # 'text': response.xml('//*[@class="itemContentText"]//table//text()').extract_first(),
-            # 'tags': quote.css('div.tags a.tag::text').extract(),
+            'title': response.css('h4.fw_b::text').extract(),
+            'location': response.css('div.item-facets2 > font.hidden-xs::text').extract(),
+            'price': response.css('span.list-price-value::text').extract(),
         }
